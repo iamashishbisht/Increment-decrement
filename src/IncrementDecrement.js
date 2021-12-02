5141dfd434dc75b5df5244cbf9aac26a20f4de20
iamashishbisht <iamashishbisht@gmail.com>
2021-12-02 22:17:45 +0530

    Increment functionality has been added

---
+++ src/IncrementDecrement.js
@@ -0,0 +1,48 @@
+
+import React, {useState} from 'react';
+
+
+let counter =5;
+
+
+
+
+
+const IncDec = ()=>{
+    const [state, setState] = useState(5);
+//1
+    // function incrementCounter(){
+
+    //     setState(state + 1);
+    //     //counter = counter + 1;
+    //     //console.log(counter);
+        
+    // }
+//2
+    // function incrementCounter(){
+
+    //  setState(prevState=> 
+    //  prevState + 1 );
+    //  }
+//3
+function incrementCounter(){
+
+    setState(prevState=> {
+    return prevState + 1 
+});
+    }
+
+console.log("INC");
+    return(
+<>
+{//4 /* <button onClick= {()=> setState(state + 1)}>+</button> */ 
+}
+<button onClick= {incrementCounter}>+</button>
+{state}
+<button>-</button>
+</>
+
+    );
+}
+
+export default IncDec;
